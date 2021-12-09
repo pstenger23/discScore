9568b81e21b4fae320d582b3043cfcbaba2968fc
P Stenger <pstenger23@gmail.com>
2021-12-09 16:20:00 -0600

    localsotrage functionality complete for score card

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,33 +1,9 @@
 
-// let updateScore1 = localStorage.getItem("p1Score");
-// let updateScore2 = localStorage.getItem("p2Score");
-// let updateScore3 = localStorage.getItem("p3Score");
-// let updateScore4 = localStorage.getItem("p4Score");
-// let currentScoreP1 = updateScore1;
-// let currentScoreP2 = updateScore2;
-// let currentScoreP3 = updateScore3;
-// let currentScoreP4 = updateScore4;
-
-
 // SET SCORES
 let currentScoreP1 = 0;
 let currentScoreP2 = 0;
 let currentScoreP3 = 0;
 let currentScoreP4 = 0;
-
-// if(updateScore1 === null) {
-//     currentScoreP1 = 0
-// }
-// if(updateScore2 === null) {
-//     currentScoreP2 = 0
-// }
-// if(updateScore3 === null) {
-//     currentScoreP3 = 0
-// }
-// if(updateScore4 === null) {
-//     currentScoreP4 = 0
-// }
-
 
 //GET P1 SCORE INCREASE/DECREASE
 const scorePlus1 = document.getElementById('p1plus');
@@ -66,60 +42,26 @@ p4score.textContent = currentScoreP4;
 
 //UPDATE ARRAY1 WITH LOCALSTORAGE
 let savedScore1 = [];
-let split = []
 const getScore1 = localStorage.getItem('p1score');
-
-
-
-// if(getScore1 === null) {
-//     savedScore1 = []
-// }
-// else {
-    
-//     // console.log(getScore1)
-//     for(i =0; i < getScore1.length; i++) {
-
-//         savedScore1.push(getScore1[i]);
-//     }
-// }
 
 //UPDATE ARRAY2 WITH LOCALSTORAGE
 let savedScore2 = [];
-
 const getScore2 = localStorage.getItem('p2score');
 
-
-if(getScore2 === null) {
-    savedScore2 = []
-}
-else {
-    getScore2.toString();
-    // console.log(getScore2);
-    for(i =0; i < getScore2.length; i++) {
-        getScore2.split(',');
-        // console.log(getScore2);
-        savedScore1.push(getScore2[i]);
-    }
-}
 //UPDATE ARRAY3 WITH LOCALSTORAGE
 let savedScore3 = [];
 const getScore3 = localStorage.getItem('p3score');
-if(getScore3 === null) {
-    savedScore3 = [];
-}
-else {
-    for(i = 0; i <= getScore3.length; i += 2) {
-        savedScore3.push(getScore3[i]);
-    }
-}
+
+//UPDATE ARRAY4 WITH LOCALSTORAGE
 let savedScore4 = [];
+const getScore4 = localStorage.getItem('p4score');
 
 //P1 ADD SCORE TO LIST
 addScoreBtn1.onclick = function() {
     const newScore1 = document.createElement('li');
 
     newScore1.textContent = currentScoreP1;
-    // console.log(newScore1);
+
     scoreList1.appendChild(newScore1);
     
     
@@ -137,9 +79,9 @@ addScoreBtn2.onclick = function() {
     scoreList2.appendChild(newScore2);
 
     saveScoreStorage2(newScore2);
-
+    
     currentScoreP2 = 0;
-    p1score.textContent = currentScoreP2;
+    p2score.textContent = currentScoreP2;
 }
 //P3 ADD SCORE TO LIST
 addScoreBtn3.onclick = function() {
@@ -152,7 +94,7 @@ addScoreBtn3.onclick = function() {
     saveScoreStorage3(newScore3);
 
     currentScoreP3 = 0;
-    p1score.textContent = currentScoreP3;
+    p3score.textContent = currentScoreP3;
 }
 //P4 ADD SCORE TO LIST
 addScoreBtn4.onclick = function() {
@@ -162,10 +104,10 @@ addScoreBtn4.onclick = function() {
 
     scoreList4.appendChild(newScore4);
 
-    saveScoreStorage(newScore4);
+    saveScoreStorage4(newScore4);
 
     currentScoreP4 = 0;
-    p1score.textContent = currentScoreP4;
+    p4score.textContent = currentScoreP4;
 }
 //P1 SCORE INCREASE
 scorePlus1.onclick = function(event) {
@@ -237,54 +179,68 @@ scoreMinus4.onclick = function(event) {
 const saveScoreStorage1 = function(newScore1) {
     
     savedScore1.push(newScore1.textContent);
-    // console.log(savedScore1);
+
     localStorage.setItem('p1score', savedScore1);
     
 }
 const saveScoreStorage2 = function(newScore2) {
     
     savedScore2.push(newScore2.textContent);
+
     localStorage.setItem('p2score', savedScore2);
     
 }
 const saveScoreStorage3 = function(newScore3) {
+    
     savedScore3.push(newScore3.textContent);
+    
     localStorage.setItem('p3score', savedScore3);
 
 }
+const saveScoreStorage4 = function(newScore4) {
+
+    savedScore4.push(newScore4.textContent);
+
+    localStorage.setItem('p4score', savedScore4);
+
+}
 const loadScores = function() {
-    console.log(getScore1);
-    const loadScore3 = localStorage.getItem('p3score');
-    const loadScore4 = localStorage.getItem('p4score');
-    if(getScore1 != null) {
-        
+
+    if(getScore1 != null) {   
         savedScore1.push(getScore1.split(','));
         
-        //console.log(savedScore1);
-        // console.log(getScore1.split(','));
-        
         for(i = 0; i<savedScore1[0].length; i++) {
-            console.log(savedScore1[0][i]);
             const updateScoreItem1 = document.createElement('li');
             updateScoreItem1.textContent = savedScore1[0][i];
             scoreList1.appendChild(updateScoreItem1);
-        }
-        
+        }    
     }
 
-    // if(loadScore2 != null) {
+    if(getScore2 != null) {
+        savedScore2.push(getScore2.split(','));
+        
+        for(i = 0; i <savedScore2[0].length; i++) {
+            const updatedScoreItem2 = document.createElement('li');
+            updatedScoreItem2.textContent = savedScore2[0][i];
+            scoreList2.appendChild(updatedScoreItem2);
+        }
+    }
+    if(getScore3 != null) {
+        savedScore3.push(getScore3.split(','));
 
-    //     for(i = 0; i < loadScore2.length; i++) {
-    //         const reLoadScoresP2 = document.createElement('li');
-    //         reLoadScoresP2.textContent = loadScore2[i];
-    //         scoreList2.appendChild(reLoadScoresP2);
-    //     }
-    // }
-    if(loadScore3 != null) {
-        for(i = 0; i <= loadScore3.length; i += 2) {
-            const reLoadScoresP3 = document.createElement('li');
-            reLoadScoresP3.textContent = loadScore3[i];
-            scoreList3.appendChild(reLoadScoresP3);
+        for(i = 0; i < savedScore3[0].length; i++) {
+            const updatedScoreItem3 = document.createElement('li');
+            updatedScoreItem3.textContent = savedScore3[0][i];
+            scoreList3.appendChild(updatedScoreItem3);
+        }
+    }
+    if(getScore4 != null) {
+        savedScore4.push(getScore4.split(','));
+
+        for(i = 0; i < savedScore4[0].length; i++) {
+            const updatedScoreItem4 = document.createElement('li');
+            updatedScoreItem4.textContent = savedScore4[0][i];
+            scoreList4.appendChild(updatedScoreItem4);
         }
     }
     
